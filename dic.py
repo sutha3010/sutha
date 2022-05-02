ad0e6c7b8ae4b80361eb10ad6408ac9adbc94a67
sutha3010 <83632596+sutha3010@users.noreply.github.com>
2022-05-01 22:34:24 -0700

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,5 @@
+synonym_dict={'habitat':['environment','surroundings','territory','home'],'clean':['unsoiled','tidy','speckless','unsullied','unstained']}
+print('My synonym Dictionary:')
+print('\n')
+print('Word List')
+print('________________')
